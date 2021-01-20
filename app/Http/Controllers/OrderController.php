42bfedfc60cd97a1e38d130e3f51236ea0a7917f
otol <satriotol69@gmail.com>
2021-01-21 03:33:12 +0700

    ctreate4

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -13,7 +13,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        return view('adminlte.index');
+        return view('order.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class OrderController extends Controller
      */
     public function create()
     {
-        //
+        return view('order.create');
     }
 
     /**
