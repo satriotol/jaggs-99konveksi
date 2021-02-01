06cb0d01ee1eef71022f14286f631e55cc134651
otol <satriotol69@gmail.com>
2021-02-02 00:15:37 +0700

    login interface bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,9 +57,9 @@
     <script type="text/javascript">
         // Initialize the service worker
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('{{asset('serviceworker.js ')}}', {
-                    scope: '.'
-                }).then(function (registration) {
+            navigator.serviceWorker.register('{{asset("serviceworker.js")}}', {
+                scope: '.'
+            }).then(function (registration) {
                 // Registration was successful
                 console.log('Laravel PWA: ServiceWorker registration successful with scope: ', registration
                     .scope);
