465a6ce63206100a30fcc6b4f3bd9eeee3160cbd
Satrio <satriotol69@gmail.com>
2021-02-03 22:01:54 +0700

    delete contact

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -106,9 +106,6 @@
                 <li class="nav-item d-none d-sm-inline-block">
                     <a href="{{route('home')}}" class="nav-link">Home</a>
                 </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="#" class="nav-link">Contact</a>
-                </li>
             </ul>
             <form class="form-inline ml-3">
                 <div class="input-group input-group-sm">
