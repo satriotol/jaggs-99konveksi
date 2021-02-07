cd4c48f11b431798f2adc03186c55d6605d10635
Satrio <satriotol69@gmail.com>
2021-02-07 20:28:51 +0700

    href home to user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -68,7 +68,7 @@
                         <div class="icon">
                             <i class="ion ion-person-add"></i>
                         </div>
-                        <a href="" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{route('user.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 @endif
