036e3563ad0ba7af6f9c84ef6197a8ee122ac8df
Satrio <satriotol69@gmail.com>
2021-02-07 20:26:33 +0700

    cancel

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -97,7 +97,7 @@
         </div>
         <div class="row">
             <div class="col-12">
-                <a href="#" class="btn btn-secondary">Cancel</a>
+                <a href="{{url()->previous()}}" class="btn btn-secondary">Cancel</a>
                 <input type="submit" value="Register" class="btn btn-success float-right">
             </div>
             </form>
