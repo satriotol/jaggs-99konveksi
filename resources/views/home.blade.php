302499e6d946ef03c3a0070c2d5171379e86bc85
satriotol <satriotol69@gmail.com>
2021-02-15 10:53:24 +0700

    your order brow

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,20 +42,22 @@
                     </div>
                 </div>
                 <!-- ./col -->
-                {{-- <div class="col-lg-3 col-6">
+                @if (auth()->user()->isAdmin())
+                <div class="col-lg-3 col-6">
                     <!-- small box -->
                     <div class="small-box bg-success">
                         <div class="inner">
-                            <h3>53<sup style="font-size: 20px">%</sup></h3>
+                            <h3>{{$orderuser}}</h3>
 
-                            <p>Bounce Rate</p>
+                            <p>Your Order</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-stats-bars"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{route('order.your')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
-                </div> --}}
+                </div>
+                @endif
                 <!-- ./col -->
                 @if (auth()->user()->isAdmin())
                 <div class="col-lg-3 col-6">
