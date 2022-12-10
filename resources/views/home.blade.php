9af4d712bf38f7272c75e8f21afe6964b33b80fb
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-12-10 22:19:28 +0700

    fixing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
         <div class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
@@ -58,10 +56,4 @@
                 <!-- Main row -->
         </section>
     </div>
-    <!-- /.row (main row) -->
-    </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-    </div>
-    <!-- ./wrapper -->
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -107,21 +107,9 @@
                         <a href="{{ route('home') }}" class="nav-link">Home</a>
                     </li>
                 </ul>
-                <form class="form-inline ml-3">
-                    <div class="input-group input-group-sm">
-                        <input class="form-control form-control-navbar" type="search" placeholder="Search"
-                            aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-navbar" type="submit">
-                                <i class="fas fa-search"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-
             </nav>
             <aside class="main-sidebar sidebar-dark-primary elevation-4">
-                <a href="index3.html" class="brand-link">
+                <a href="{{ route('home') }}" class="brand-link">
                     <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
                         class="brand-image img-circle elevation-3" style="opacity: .8">
                     <span class="brand-text font-weight-light"><b>JAGGS</b>.id</span>
